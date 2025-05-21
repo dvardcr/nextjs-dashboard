a184b9c43add0cbf487d302eadf35937b21eaed9
Diego Vargas <diego.vargasd@gmail.com>
2025-05-20 23:39:58 -0600

    Fixed most of the lint errors given

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -54,10 +54,8 @@ export async function createInvoice(prevState: State, formData: FormData) {
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
     } catch (error) {
-        // console.error(error);
-        return {
-            message: 'Database Error: Failed to Create Invoice.',
-    };
+        console.error(error);
+        //return {message: 'Database Error: Failed to Create Invoice.'};
 }
 
     revalidatePath('/dashboard/invoices');
@@ -86,8 +84,8 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
         WHERE id = ${id}`;
     } catch (error) {
-        // console.error(error);
-        return { message: 'Database Error: Failed to Update Invoice.' };
+        console.error(error);
+        // return { message: 'Database Error: Failed to Update Invoice.'};
     }
 
     revalidatePath('/dashboard/invoices');
